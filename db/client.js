4e537904bfd1cda5716896512ecda89ea2cc0000
MarcMarsal <59292333+MarcMarsal@users.noreply.github.com>
2026-06-10 09:00:21 +0200

    Update client.js

--- db/client.js
+++ db/client.js
@@ -1,5 +1,9 @@
 // db/client.js
+import pg from "pg";
 import { Client } from "pg";
+
+// 🔥 Convertir float8 (double precision) a número real
+pg.types.setTypeParser(701, parseFloat); // 701 = OID de float8
 
 export const client = new Client({
   connectionString: process.env.DATABASE_URL,
