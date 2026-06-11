30b6fb4dc943bd2c1922862cd2da5228cff01da8
MarcMarsal <59292333+MarcMarsal@users.noreply.github.com>
2026-06-11 08:45:58 +0200

    Update send.js

--- telegram/send.js
+++ telegram/send.js
@@ -1,30 +1,13 @@
 // telegram/send.js
 import axios from "axios";
 
-export async function sendTelegram({
-  bot = "",
-  symbol = "",
-  timeframe = "",
-  signalType = "",
-  color = "",
-  entry = "",
-  tp = "",
-  sl = ""
-}) {
-  const url = `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`;
+export async function sendTelegram(text) {
+ 
+const url = `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`;
 
-  let message = "";
-  if (bot) message += `Bot: <b>${bot}</b>\n`;
-  if (symbol) message += `<b>${symbol} ${timeframe}</b>\n`;
-  if (signalType) message += `Tipus: <b>${signalType}</b>\n`;
-  if (color) message += `Color: <b>${color}</b>\n`;
-  if (entry) message += `Entrada: <b>${entry}</b>\n`;
-  if (tp) message += `TP: <b>${tp}</b>\n`;
-  if (sl) message += `SL: <b>${sl}</b>\n`;
-  //console.log(message);
   const payload = {
     chat_id: process.env.TELEGRAM_CHAT_ID,
-    text: message,
+    text,
     parse_mode: "HTML"
   };
 
